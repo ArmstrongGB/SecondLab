a4b7d6fc89256b5a89604cc3775a74d8461aa71d
Gleb Blinov <glebblinov@yandex.ru>
2023-05-06 13:36:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/secondlab/Second.kt
@@ -0,0 +1,10 @@
+package com.example.secondlab
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+class Second : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity)
+    }
+}
